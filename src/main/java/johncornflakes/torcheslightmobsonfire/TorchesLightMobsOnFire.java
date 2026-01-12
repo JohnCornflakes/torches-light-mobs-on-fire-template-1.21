bc35d002ca12a9515416a94933263871d775cc15
JohnCornflakes <johncornflakes@yahoo.com>
2026-01-11 22:32:35 -0800

    Adds copper torch functionality

--- src/main/java/johncornflakes/torcheslightmobsonfire/TorchesLightMobsOnFire.java
+++ src/main/java/johncornflakes/torcheslightmobsonfire/TorchesLightMobsOnFire.java
@@ -32,7 +32,6 @@ public class TorchesLightMobsOnFire implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
 
 //        AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
 //            Item handItem = player.getMainHandStack().getItem();
@@ -45,13 +44,12 @@ public class TorchesLightMobsOnFire implements ModInitializer {
 //        });
 
         ServerLivingEntityEvents.ALLOW_DAMAGE.register((livingEntity, damageSource, v) -> {
-            LOGGER.info(livingEntity.toString());
             Entity attacker = damageSource.getAttacker();
 
-            if (attacker instanceof ZombieEntity || attacker instanceof PlayerEntity || attacker instanceof HuskEntity || attacker instanceof DrownedEntity) {
-                LOGGER.info(attacker.toString());
+            //            if (attacker instanceof ZombieEntity || attacker instanceof PlayerEntity || attacker instanceof HuskEntity || attacker instanceof DrownedEntity) {
+            if (attacker instanceof LivingEntity) {
                 Item itemInMain = ((LivingEntity) attacker).getMainHandStack().getItem();
-                if (itemInMain == Items.TORCH || itemInMain == Items.SOUL_TORCH) {
+                if (itemInMain == Items.TORCH || itemInMain == Items.SOUL_TORCH || itemInMain == Items.COPPER_TORCH) {
                     livingEntity.setFireTicks(120);
                 }
             }
